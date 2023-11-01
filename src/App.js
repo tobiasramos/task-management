974ca750935eefc4dcebd7f525796d1fd81ca7c9
Tobias <tobiasramossilva@gmail.com>
2023-11-01 19:14:27 -0300

    Criando a tela de login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Register from "./pages/register/register";
 import axios from "axios";
+import Login from "./pages/login/login";
 axios.defaults.baseURL = "http://localhost:3001/";
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
