5e1a259876a0a8d06f594910c89ca00c3a00295d
Tobias <tobiasramossilva@gmail.com>
2023-11-01 23:43:25 -0300

    Fazendo o login do usuário para a tela home, faltando ainda o id para que cada usuário tenha sua própria tela home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Register from "./pages/register/register";
+import Home from "./pages/home/home";
 import axios from "axios";
 import Login from "./pages/login/login";
 axios.defaults.baseURL = "http://localhost:3001/";
@@ -8,12 +9,11 @@ axios.defaults.baseURL = "http://localhost:3001/";
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store";
+import { Toaster } from "react-hot-toast";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            success: {
+              style: {},
+            },
+            error: {
+              style: {},
+            },
+            style: {},
+          }}
+        />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
-
