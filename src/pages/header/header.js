c1368ed10b056c49eabae331cc7f33fd171767d1
Tobias <tobiasramossilva@gmail.com>
2023-11-03 16:27:42 -0300

    Fazendo o logout.

--- src/pages/header/header.js
+++ src/pages/header/header.js
@@ -1,12 +1,24 @@
 import "./header.css";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../redux/actions";
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+    window.location.href = "/login";
+  };
+
   return (
     <div className="header-container">
       <img src="./img/logo.png" alt="Logo da aplicação" />
       <div className="info">
         <span>tobias@gmail.com</span>
-        <span className="exit">sair</span>
+        <span className="exit" onClick={handleLogout}>
+          sair
+        </span>
       </div>
     </div>
   );
